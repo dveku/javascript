123b0f1668028e12d2b32f7a0e5fac326a3b05c4
dveku <b.dhanush29102002@gmail.com>
2022-04-08 08:13:18 +0530

    final commit

--- aap.js
+++ aap.js
@@ -3,8 +3,29 @@ var txtInput = document.querySelector("#txt-input");
 var outputdiv = document.querySelector("#output");
 //output statement
 //outputdiv.innerText = "Dhanush Balivada";
+//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("something wrong with the server. Try again later");
+}
 
 function clickHandler() {
-    outputdiv.innerText = "asjasjdoweldj" + txtInput.value;
+
+    var inputText = txtInput.value;
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputdiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
 }
 btnTranslate.addEventListener("click", clickHandler);
